0a3901cb8685f8c38c275790988fd78977cbf212
Eruvala Vishnusai <Eruvala.Vishnusai@sita.aero>
2024-09-14 02:12:33 +0530

    AngularAssesment4

---
+++ cmp-databinding-assignment-start/src/app/game-control/game-control.component.ts
@@ -0,0 +1,24 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-game-control',
+  templateUrl: './game-control.component.html',
+  styleUrl: './game-control.component.css'
+})
+export class GameControlComponent {
+
+  @Output() intervalFired = new EventEmitter<number>();
+  interval: any;
+  lastNumber = 0;
+
+  onStartGame() {
+    this.interval = setInterval(() => {
+      this.intervalFired.emit(this.lastNumber + 1);
+      this.lastNumber++;
+    }, 1000);
+  }
+
+  onStopGame() {
+    clearInterval(this.interval);
+  }
+}
